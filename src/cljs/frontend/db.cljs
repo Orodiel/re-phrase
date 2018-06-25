f89a0c963646c2ffbd3d5d4483cbe694f6e75d82
Eugene <orodiel@gmail.com>
2018-06-26 01:28:53 +0300

    fixed wrong url-function

--- src/cljs/frontend/db.cljs
+++ src/cljs/frontend/db.cljs
@@ -3,10 +3,13 @@
 
 (def default-db
   {:login      {:input ""}
+
    :user       {:name ""}
    :chat       {:input    ""
                 :messages (sorted-map-by >)}
 
    :connection {:socket     nil
                 :connected? false
-                :url-fn     #(str "ws://localhost:8080/chat/" (:name %))}})
+                :url-fn     #(-> (.-location js/document)
+                                 (str "chat/" (:name %))
+                                 (replace "http" "ws"))}})
